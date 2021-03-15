2c91bbfa9bf327abd4ee8c2a0cae9ff62abb8f60
Ray Messina <rmessina10@gmail.com>
2021-03-15 02:24:25 -0500

    useState Hook demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+  const [color, setColor] = useState('red');
+  const [msg, setMsg] = useState('');
+
+  function handleClick() {
+    color === 'red' ? setColor('blue') : setColor('red');
+    setMsg('Super Secret Message');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ background: color }}>React Hooks Demo</h1>
+      <h1>{msg}</h1>
+      <button onClick={handleClick}>Click me</button>
     </div>
   );
 }
