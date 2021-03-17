079693e7a6c30567214e47033542bac2be7c0170
Ray Messina <rmessina10@gmail.com>
2021-03-16 23:49:57 -0500

    Intro to useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
+
+function ExampleChild() {
+
+  useEffect(() => {
+    console.log('Subscribing...(effect)')
+    return () => {
+      console.log('Unsubscribing...(cleanup)')
+    }
+  });
+
+  return (
+    <h2>I am the example child</h2>
+  );
+}
 
 function App() {
   const [color, setColor] = useState('red');
-  const [msg, setMsg] = useState('');
+  const [msg, setMsg] = useState('Initial Message');
 
-  function handleClick() {
+  // useEffect(() => {
+  //   console.log(msg);
+  // }, [msg, color]);
+
+  // useEffect(() => {
+  //   console.log('mount');
+  // }, []);//empty array prevents effect from running after mounting, thus simulating componentDidMount();
+
+
+  function changeColor() {
     color === 'red' ? setColor('blue') : setColor('red');
+  }
+  function changeMsg() {
     setMsg('Super Secret Message');
   }
 
@@ -14,7 +39,9 @@ function App() {
     <div>
       <h1 style={{ background: color }}>React Hooks Demo</h1>
       <h1>{msg}</h1>
-      <button onClick={handleClick}>Click me</button>
+      <button onClick={changeColor}>Click me(color)</button>
+      <button onClick={changeMsg}>Click me(msg)</button>
+      {msg === 'Initial Message' && <ExampleChild />}
     </div>
   );
 }
